1818cabe394eda2854f28425e57df1ac8c803566
Cameron Ellis <ce325@milgram1.milgram.hpc.yale.internal>
2020-10-10 15:27:53 -0400

    Give functionality to do z scoring within volume

--- prototype/link/scripts/zscore_nii.sh
+++ prototype/link/scripts/zscore_nii.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 # Z scores a nifti through time. Takes as an input the name of the functional to be z scored and also then the name of the output. 
-# This script shouldn't be used in most circumstances because z_score_exclude.m accounts for TRs that are excluded, but nonetheless it is still here
+# This script shouldn't be used in most circumstances because z_score_exclude.m accounts for TRs that are excluded, but nonetheless it is still here.
+# It can be useful if you just want to z score a single volume
 #
 # Ellis 2/14/17	
 
@@ -14,9 +15,31 @@ fi
 Input=$1
 Output=$2
 
+# Get the number of volumes
+n_vols=`fslnvols $Input`
+
+if [ $n_vols -gt 1 ]
+then
+
 fslmaths $Input -Tstd temp.nii.gz
 
 #Generate the normalized volume
 fslmaths $Input -Tmean -sub $Input -div	temp.nii.gz -mul -1 $Output
 
+else
+
+echo Found one volume, zscoring within volume
+
+mean_val=`fslstats $Input -M`
+sd_val=`fslstats $Input -S`
+
+fslmaths $Input -sub $mean_val -div $sd_val $Output
+
+# Mask the data
+fslmaths $Input -abs -bin temp.nii.gz
+fslmaths $Output -mas temp.nii.gz $Output
+
+fi
+
+# Delete temporary volume
 rm -f temp.nii.gz
