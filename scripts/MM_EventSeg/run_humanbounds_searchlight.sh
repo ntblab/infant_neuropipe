9dabfdb3b80ab9b56812e833c13d587d8103e34a
TRISTAN <tristan.yates@yale.edu>
2021-11-28 14:09:39 -0500

    source globals

--- scripts/MM_EventSeg/run_humanbounds_searchlight.sh
+++ scripts/MM_EventSeg/run_humanbounds_searchlight.sh
@@ -8,8 +8,11 @@
 #SBATCH --mem-per-cpu 25G        # memory limit
 #SBATCH -n 5        # how many cores to use
 
-# Load the modules
-source globals.sh
+# Set up the environment
+module load Python/Anaconda3
+module load brainiak/0.8-Python-Anaconda3
+module load nilearn/0.5.0-Python-Anaconda3
+module load OpenMPI/2.1.2-GCC-6.4.0-2.28
 
 age=$1
 sub=$2
